2c639599fff3febae89b57214d18a40810a85938
Agnes Kabongo <57146479+agkabongo@users.noreply.github.com>
2021-03-02 18:55:26 +0100

    price change

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
                 <div className="details">
                     <p>Type: {activities.type}</p>
                     <p>Participants: {activities.participants}</p>
-                    <p>Price: {activities.price}$</p>
+                    <p>Price: {activities.price*10}$</p>
                     <a href={activities.link}>GO</a>
 
                 </div>
